6b070dcce335c9c116ec10e809f345a6234b71e0
Shobhan Satpathy <165057253+Shobhan-04@users.noreply.github.com>
2026-09-22 05:03:02 +0530

    Time: 88 ms (12.61%), Space: 74.3 MB (61.09%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -4,23 +4,24 @@ class Solution {
             Using minHeap :-
                 Time complexity = O(m*log(n)), 
                 Space complexity = O(1)
+            where m is the size of minHeap.
         */
 
-        int n = nums.length;
+        int n = nums.length; // no. of elements in nums array.
         Arrays.sort(nums); // Sort the array.
 
         PriorityQueue<Integer> minHeap = new PriorityQueue<>(); // Maintain a minHeap.
-        int result = 0;
+        int result = 0; // result used to store the kth largest elment in the nums array.
 
-        for(int i = 0; i < n; i++){
-            int val = nums[i];
+        for(int i = 0; i < n; i++){ // Iterate over the nums length.
+            int val = nums[i]; // get each element from the nums array.
             minHeap.offer(val); // Add elements into the minHeap one-by-one.
 
-            int m = minHeap.size();
+            int m = minHeap.size(); // get the size of the minHeap.
             if(m > k) minHeap.poll(); // Remove the top most element from the minHeap.
         }
 
         result = minHeap.peek(); // Display the peek element in the minHeap,
-        return result;
+        return result; // Return the kth largest element in the array.
     }
 }
