e842b83d774cb331f93abc14c61f00f5a769b70a
Shobhan Satpathy <165057253+Shobhan-04@users.noreply.github.com>
2026-09-22 05:46:11 +0530

    Time: 37 ms (22.6%), Space: 52.7 MB (67.12%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.java
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.java
@@ -3,21 +3,23 @@ class Solution {
         /*
             Using minHeap :-
                 Time Complexity: O(n * l * log(k)), where n is the number of elements in the array and l is the maximum length of a string.
+                
                 Space Complexity: O(k * l) to store upto k string elements in the loop.
         */
 
-        int n = nums.length;
+        int n = nums.length; // no. of elments in nums array.
 
         Arrays.sort(nums); // Sort the array.
 
+        // Create a Min-Heap of Strings using a custom comparator :-
         PriorityQueue<String> minHeap = new PriorityQueue<>((a, b) -> {
             int aLen = a.length(), bLen = b.length();
 
-            if(aLen != bLen){
-                return Integer.compare(aLen, bLen);
+            if(aLen != bLen){ // check if the both strings length does'nt match.
+                return Integer.compare(aLen, bLen); // Shorter string gets added first in minHeap.
             }
 
-            return a.compareTo(b);
+            return a.compareTo(b); // Lexicogrphically compare both the strings if in case the length matches.
         }); 
 
         String kthLargestInt = ""; // result used to store the kth largest elment in the nums array.
@@ -27,7 +29,7 @@ class Solution {
             minHeap.offer(val); // Add elements into the minHeap one-by-one.
 
             int m = minHeap.size(); // get the size of the minHeap.
-            if(m > k) minHeap.poll(); // // 3. If heap size exceeds k, remove the smallest element from minHeap.
+            if(m > k) minHeap.poll(); // If heap size exceeds k, remove the smallest element from minHeap.
         }
 
         kthLargestInt = minHeap.peek(); // Display the peek element in the minHeap.
